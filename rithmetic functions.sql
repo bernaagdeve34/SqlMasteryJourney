dd0db730d57079ff770b52547bc9617b2767d4a6
Berna Ağdeve <158767394+bernaagdeve34@users.noreply.github.com>
2025-12-23 14:17:34 +0300

    rithmetic functions

---
+++ rithmetic functions.sql
@@ -0,0 +1,19 @@
+select *from bolum3
+select id,replace(ad,'i','ı') from bolum3
+select reverse(ad) from bolum3
+select substring('Berna Agdeve',4,2)
+select lower(ad) from bolum3
+select upper(ad) from bolum3
+select(-5)
+select abs(-5)
+select (4.85)
+select ceil(4.85)
+select floor(4.85)
+select pi()
+select power(2,4)
+select Random()
+select Round(18.12345,2)
+select sign(15)
+select sign(-15)
+select sqrt(363)
+select log(100)
